d198a4b8cce7c42b8a5b2ba7b510ffa74303ef69
vladshup <qfminbox@gmail.com>
2020-12-26 11:22:59 +0300

    buttons library changed

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
-PowerSDR mrx (Thetis) MIDI surface for STM32F103C8T6
+PowerSDR mrx (Thetis) MIDI surface for STM32F103C8T6 "blue pill" board.
 
-Weeel's, button's, key's.
+Support for encoders's, button's (with LongPress), potentiometer's via MIDI.
 
 Setup process - https://www.youtube.com/watch?v=X81hrYqUJiQ&t
--- STM32_gr_dec_poty_keys_debounce_midi.ino
+++ STM32_gr_dec_poty_keys_debounce_midi.ino
@@ -1,22 +1,33 @@
-#include <ButtonDebounce.h>
+
 /* Rotary encoder grey code read lib only for PowerSDR(Thetis)*/
-#include <MidiEncoder.h>
-#include <USBComposite.h>
 #define STM32DUINO_CORE
+#include <USBComposite.h>
+#include <MidiEncoder.h>
+#include <MidiPot.h>
+#include <AceButton.h>
+using namespace ace_button;
 
-//Buttons debounce delay ms
-#define DEBOUNCE 100
+// The pin numbers attached to the buttons.
+const int RIT_BUTTON_PIN = PB4;
+const int SPLIT_BUTTON_PIN = PB11;
+const int AB_BUTTON_PIN = PB10;
 
-//OOP Wheel encoders 5V-GND
-const unsigned int enc0_command = 40; //encoder 0 ID
-MidiEncoder enc0(PC14, PC15); //Arg's - STM32 pins
-/*
-const unsigned int enc1_command = 41; //encoder 1 ID
-MidiEncoder enc1(PB03, PB04);//Arg's - STM32 pins
-*/
+// Buttons, automatically sharing the default System ButtonConfig.
+AceButton ritButton(RIT_BUTTON_PIN);
+AceButton splitButton(SPLIT_BUTTON_PIN);
+AceButton abButton(AB_BUTTON_PIN);
 
-//Main encoder speed divider
-#define SDIV 4
+//Encoders 5V-GND
+const unsigned int vfoA_id = 40; //encoder 0 ID
+MidiEncoder vfoA(PC14, PC15); //Arg's - STM32 pins
+#define VFO_A_DIV 2
+
+const unsigned int rit_id = 41; //encoder 1 ID
+MidiEncoder rit(PB6, PB7);//Arg's - STM32 pins
+
+
+//RIT/XIT encoder speed mult
+#define MULT 1
 
 USBMIDI midi;
 const unsigned int midi_channel = 4; // this might show up as channel 1 depending on start index
@@ -39,28 +50,62 @@ myMidi midiin; //Fake midi input emulation
 //Potentiometrs 3.3V-GND
 const uint8 threshold = 1;
 
-#define POT_0 PA0 //ADC pins: PA0-PA7,PB0,PB1
-const unsigned int pot0_command = 50; // poty 0 ID
-unsigned int pot0_old_value = 0;
-unsigned int pot0_new_value = 0;
+//ADC pins: PA0-PA7,PB0,PB1
+const unsigned int pot1_id = 50; // poty 1 ID
+MidiPot pot1(PA0);
 
 /*
-#define POT_1 PA1 //ADC pins: PA0-PA7,PB0,PB1
-const unsigned int pot1_command = 51; // poty 1 ID
-unsigned int pot1_old_value = 0;
-unsigned int pot1_new_value = 0;
+const unsigned int pot2_id = 51; // poty 2 ID
+MidiPot pot2(PA1);
 */
 
-//Buttons
-const unsigned int key0_command = 60; // button 0
-#define KEY_0 PB11 //Any digital pin
-unsigned int key00 = 0;
-ButtonDebounce key0(KEY_0, DEBOUNCE);
 
-const unsigned int key1_command = 61; // button 1
-#define KEY_1 PB10 //Any digital pin
-unsigned int key01 = 0;
-ButtonDebounce key1(KEY_1, DEBOUNCE);
+// The event handler for the buttons.
+void handleEvent(AceButton* button, uint8_t eventType,
+    uint8_t /*buttonState*/) {
+  uint8_t pin = button->getPin();
+
+  if (pin == RIT_BUTTON_PIN) {
+    switch (eventType) {
+      case AceButton::kEventPressed:
+        midi.sendControlChange(midi_channel, 60, 127);
+      break;
+            
+      case AceButton::kEventReleased:
+        midi.sendControlChange(midi_channel, 60, 0);
+      break;
+
+      // LongPressed goes in and out of edit mode.
+      case AceButton::kEventLongPressed:
+      midi.sendControlChange(midi_channel, 61, 127);
+      break;
+    }
+  } else if (pin == SPLIT_BUTTON_PIN) {
+    switch (eventType) {
+      case AceButton::kEventReleased:
+        midi.sendControlChange(midi_channel, 62, 0);
+        break;
+
+      case AceButton::kEventPressed:
+        midi.sendControlChange(midi_channel, 62, 127);
+      break;
+    }
+  } else if (pin == AB_BUTTON_PIN) {
+
+    switch (eventType) {
+      case AceButton::kEventReleased:
+        midi.sendControlChange(midi_channel, 63, 0);
+        break;
+
+      case AceButton::kEventPressed:
+        midi.sendControlChange(midi_channel, 63, 127);
+      break;
+    }    
+  }
+  
+}// End the event handler for the buttons.
+
+
 
 void setup()
 {
@@ -68,77 +113,58 @@ void setup()
     USBComposite.setProductId(0x0030);
     pinMode(SPEAKER_PIN, OUTPUT);
     midiin.registerComponent();
-    USBComposite.begin();
-  
-    pinMode(POT_0, INPUT);
+    USBComposite.begin();  
     midi.begin();
-    delay(1000);
+
+  // Button uses the built-in pull up register.
+  pinMode(RIT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(SPLIT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(AB_BUTTON_PIN, INPUT_PULLUP);
+
+  // Configure the ButtonConfig with the event handler.
+  ButtonConfig* buttonConfig = ButtonConfig::getSystemButtonConfig();
+  buttonConfig->setEventHandler(handleEvent);
+  buttonConfig->setFeature(ButtonConfig::kFeatureLongPress);
+  buttonConfig->setFeature(ButtonConfig::kFeatureRepeatPress);
+  buttonConfig->setFeature(ButtonConfig::kFeatureSuppressAfterLongPress);
+    
+  delay(1000);
 }//End setup
 
 
 void loop()
 {
+//Input channel  
   midiin.poll(); //Not used but need for PowerSDR(Thetis)
-
-    //Encoder wheel utility     
+  
+//Buttons
+//Don't remember add new buttons to event handler
+  ritButton.check();
+  splitButton.check();
+  abButton.check();
+  
+//Encoders  
+    //VFO_A encoder
        static int8_t pulse;
        pulse++;
-       int8_t tmpdata = enc0.read();
-       
+       int8_t tmpdata = vfoA.read();       
            if( tmpdata ) {
-
-            if ( pulse % SDIV == 0) {
-             midi.sendControlChange(midi_channel, enc0_command, tmpdata);
+            if ( pulse % VFO_A_DIV == 0) {
+             midi.sendControlChange(midi_channel, vfoA_id, tmpdata);
              pulse = 0;
             }
            }
 
-    //Buttons utility
-    key0.update();
-    int8_t old_key0;
-    old_key0 = key00;
-    key00 = key0.state();
-    if (key00 == old_key0) {  
-    } else {
-      midi.sendControlChange(midi_channel, key0_command, key00*127);  
-    }
-    
-    key1.update();
-    int8_t old_key1;
-    old_key1 = key01;
-    key01 = key1.state();
-    if (key01 == old_key1) {  
-    } else {
-      midi.sendControlChange(midi_channel, key1_command, key01*127);  
-    }
-    
-    //Poty utility
-      int temp = analogRead(POT_0); // a value between 0-4095
-      pot0_new_value = temp / 32;          // convert to a value between 0-127
-    
-      // If difference between new_value and old_value is grater than threshold
-      if ((pot0_new_value > pot0_old_value && pot0_new_value - pot0_old_value > threshold) ||
-          (pot0_new_value < pot0_old_value && pot0_old_value - pot0_new_value > threshold)) {
-    
-        midi.sendControlChange(midi_channel, pot0_command, pot0_new_value);
-    
-        // Update old_value
-        pot0_old_value = pot0_new_value;
-      }
- 
+    //RIT encoder   
+       int8_t tmpdata1 = rit.read();       
+           if( tmpdata1 ) {
+              for(int i = 1; i <= MULT; i++) {
+                midi.sendControlChange(midi_channel, rit_id, tmpdata1);
+              }
+           }
+   
+//Potentiometers
+    if (pot1.read()) midi.sendControlChange(midi_channel, pot1_id, pot1.value);
+    //if (pot2.read()) midi.sendControlChange(midi_channel, pot2_id, pot2.value);
+  
 } //End loop
-
-/* returns change in encoder state (-1,0,1) */
-/*
-int8_t read_encoder(int8_t enc_a, int8_t enc_b)
-{
- //int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
- int8_t enc_states[] = {0,127,1,0,1,0,0,127,127,0,0,1,0,1,127,0}; //PowerSDR mrx (Thetis) need only rotary direction 1 (CCW) and 127 (CW)
- static uint8_t old_AB = 0;
-
- old_AB <<= 2;                   //remember previous state
- //old_AB |= ( ENC_PORT & 0x03 );  //add current state
- old_AB |= ((digitalRead (enc_b) << 1) | digitalRead (enc_a));
- return ( enc_states[( old_AB & 0x0f )]);  
-} //End read_encoder()
-*/
